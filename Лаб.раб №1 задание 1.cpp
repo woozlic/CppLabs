a7b5f970350162c98de868a8c005b1db4fa8a33c
Александр Соломонов <sasha_solomonov@mail.ru>
2017-09-19 20:50:34 +0300

    Auto stash before merge of "master" and "origin/master"

--- Лаб.раб №1 задание 1.cpp
+++ Лаб.раб №1 задание 1.cpp
@@ -18,8 +18,14 @@ int main()
 	cout << "z=";
 	cin >> z;
 
+
 	a = (3 + pow(e, y) - 1) / (1 + pow(x, 2) * abs(y - tan(z)));
 	b = 1 + abs(y - x) + (pow(y - x, 2)) / 2 + (pow(y - x, 3)) / 3;
+
+	a = (1 + y) * ((x + y) / (pow(x, 2) + 4) / ((pow(e, -x - 2) + 1) / (pow(x, 2) + 4)));
+	b = (1 + cos(y - 2)) / (pow(x, 4) / 2 + pow(sin(z), 2));
+	
+
 	cout << "a=" << a << endl;
 	cout << "b=" << b << endl;
 
